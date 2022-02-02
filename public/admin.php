93495f6f595c4490178f4be74e3417ff1734fdf6
Mehedi Hasan <mehedihasan.mm9@gmail.com>
2022-02-02 10:45:54 +0600

    Initial Commit

---
+++ public/admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheets/public.css">
+    <title>Widget_Corp</title>
+</head>
+<body>
+    <div id="header">
+        <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
+        <div id="navigation">
+            &nbsp;
+        </div>
+        <div id="page">
+            <h2>Admin menu</h2>
+            <p>Welcome to the admin area</p>
+            <ul>
+                <li><a href="managed_content.php">Manage Website Content</a></li>
+                <li><a href="managed_admin.php">Manage Admin User</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="footer">Copyright 20xx, Widget Corp</div>
+</body>
+</html>
