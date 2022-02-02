36fc434390a679ba72637dc2da613e9754deeb5d
Mehedi Hasan <mehedihasan.mm9@gmail.com>
2022-02-02 14:25:58 +0600

    I've the make the page code reusable

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php require_once("../includes/functions.php") ?>
+<?php include("../includes/layouts/header.php") ?>
+<div id="main">
+    <div id="navigation">
+        &nbsp;
+    </div>
+    <div id="page">
+        <h2>Manage Content</h2>
+    </div>
+</div>
+<?php include("../includes/layouts/footer.php") ?>
