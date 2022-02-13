74097dce9819bc75123a178f7bb633848cac66c8
Mehedi Hasan <mehedihasan.mm9@gmail.com>
2022-02-13 20:30:38 +0600

    finding selected page code refactoring

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,72 +1,25 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
-
-<?php 
-    if(isset($_GET["subject"])) {
-        $selected_subject_id = $_GET["subject"];
-        $selected_page_id = null;
-    } else if($_GET["page"]) {
-        $selected_page_id = $_GET["page"];
-        $selected_subject_id = null;
-    } else {
-        $selected_subject_id = null;
-        $selected_page_id = null;
-    }
-?>
+<?php find_selected_page(); ?>
 
 <div id="main">
     <div id="navigation">
-        <ul class="subjects">
-            <?php 
-                $subject_set = find_all_subjects();
-            ?>
-            <?php 
-                // 3. Use return data if any
-                while($subject = mysqli_fetch_assoc($subject_set)) {
-                    // Output data from each row
-            ?>  
-                <?php 
-                    echo "<li ";
-                    if($subject["id"] == $selected_subject_id) {
-                        echo "class=\"selected\" ";
-                    }
-                    echo ">";
-                 ?>
-                    <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>">
-                        <?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?>
-                    </a>
-                    <?php
-                        $page_set = find_pages_for_subject($subject["id"]);
-                    ?>
-                    <ul class="pages">
-                        <?php
-                            while($page = mysqli_fetch_assoc($page_set)) {
-                        ?>  
-                            <li>
-                                <a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>">
-                                    <?php echo $page["menu_name"] ; ?>
-                                </a>
-                            </li>
-                        <?php } ?>
-                        <?php 
-                            mysqli_free_result($page_set);
-                        ?>
-                    </ul>
-                </li>
-            <?php 
-                } 
-            ?>
-            <?php 
-                // 4. Release return data
-                mysqli_free_result($subject_set);
-            ?>
-        </ul>
+        <?php
+            echo navigation($current_subject, $current_page);
+        ?>
     </div>
     <div id="page">
-        <h2>Manage Content</h2>
-        <p><?php echo $selected_subject_id; ?></p>
-        <p><?php echo $selected_page_id; ?></p>
+        <?php if($current_subject) { ?>
+            <h2>Manage Subject</h2>
+            Subject's Menu Name: <?php echo $current_subject["menu_name"]; ?>
+        <?php } else if($current_page) { ?>
+            <h2>Manage Page</h2>
+        Page's Menu Name: <?php echo $current_page["menu_name"]; ?>
+        <?php } else { ?>
+            <h2>Please select a subject or a page</h2>
+        <?php } ?>
+
     </div>
 </div>
 <?php include("../includes/layouts/footer.php") ?>
